c1d7935e806dc87d7cde75c62ee3ac9380aef5e8
Einar Hjorleifsson <einarhj@hafdruna.hafro.is>
2016-12-15 12:58:14 +0000

    seeding smooth spline, not operational

--- R/make_alk.R
+++ R/make_alk.R
@@ -9,12 +9,9 @@
 #' @export
 #'
 make_alk <- function(stodvar, SPECIES, lengthclass, vikmork, kvarnir) {
-  
-  # TODO: prior to function call, add n-column
-  #       then modify the function code to sum on the n's
-  
+
   # create a grid??
-  
+
   # may be of use
   medalle <- (lengthclass[-length(lengthclass)] + lengthclass[-1])/2
   lkey <-
@@ -26,8 +23,8 @@ make_alk <- function(stodvar, SPECIES, lengthclass, vikmork, kvarnir) {
     dplyr::select(lclass) %>%
     dplyr::distinct() %>%
     dplyr::mutate(medalle = medalle)
-  
-  
+
+
   if(missing(kvarnir)) {
     kvarnir <-
       fjolst::lesa.kvarnir(stodvar$synis.id, SPECIES, col.names = c("kyn", "kynthroski", "slaegt", "oslaegt")) %>%
@@ -35,7 +32,7 @@ make_alk <- function(stodvar, SPECIES, lengthclass, vikmork, kvarnir) {
                     aldur %in% 1:14) %>%  # should be user conrollable
       dplyr::left_join(stodvar, by = c("synis.id"))
   }
-  
+
   if(!missing(vikmork)) {
     kvarnir <-
       kvarnir %>%
@@ -43,18 +40,60 @@ make_alk <- function(stodvar, SPECIES, lengthclass, vikmork, kvarnir) {
       dplyr::filter(lengd >= minl & lengd <= maxl) %>%
       dplyr::select(-minl, -maxl)
   }
-  
+
+  # ----------------------------------------------------------------------------
+  # seeding smooth spline
+  smooth_model <- function(df) {
+    stats::smooth.spline(df$aldur ~ df$lengd, df = 2)
+  }
+
+  tmp <-
+    kvarnir %>%
+    dplyr::group_by(ar, region) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(model = purrr::map(data, smooth_model)) %>%
+    dplyr::select(ar, region, model)
+  for(i in 1:nrow(tmp)) {
+    x <-
+      dplyr::data_frame(ar = tmp$ar[[i]],
+                 region = tmp$region[[i]],
+                 medalle = medalle,
+                 aldur = round(stats:::predict.smooth.spline(tmp$model[[i]], medalle)$y),
+                 lclass = as.character(cut(medalle, breaks = lengthclass)))
+    if(i == 1) {
+      res <- x
+    } else {
+      res <- bind_rows(res, x)
+    }
+  }
+
+  # dplyrize below
+  i <- res$aldur > max(1:14)
+  if (any(i)) res$aldur[i] <- max(1:14)
+  i <- res$aldur < min(1:14)
+  if (any(i)) res$aldur[i] <- min(1:14)
+
+  # end seeding the smooth spline
+  # ----------------------------------------------------------------------------
+
   d <-
     kvarnir %>%
     dplyr::mutate(lclass = as.character(cut(lengd, breaks = lengthclass))) %>%
     dplyr::filter(!is.na(lclass)) %>%
     dplyr::group_by(ar, region, lclass, aldur) %>%
     dplyr::summarise(n = n()) %>%
-    dplyr::group_by(ar, region, lclass) %>%
+    dplyr::group_by(ar, region, lclass)
+
+  # here one should check for missing age in particular length class
+  # ...
+  # ...
+
+  d <-
+    d %>%
     dplyr::mutate(p = n/sum(n)) %>%
     dplyr::ungroup() %>%
     dplyr::tbl_df()
-  
+
   # make a grid
   g <-
     dplyr::data_frame(lengd = 0:200) %>%
@@ -73,7 +112,7 @@ make_alk <- function(stodvar, SPECIES, lengthclass, vikmork, kvarnir) {
     dplyr::left_join(d, by = c("ar", "region", "lclass", "aldur")) %>%
     dplyr::mutate(n = ifelse(is.na(n), 0, n),
                   p = ifelse(is.na(p), 0, p))
-  
-  return(d)
-  
+
+  return(list(d=d, res = res))
+
 }
